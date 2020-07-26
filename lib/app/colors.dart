7ba6952115f03fd2c2221eb585a92a7a8df1722d
basaif <iman.basaif@gmail.com>
2020-07-26 18:33:53 +0300

    Added colors.dart and strings.dart

---
+++ lib/app/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorSets {
+
+  static const Color primaryGreen = Colors.lightGreen;
+  static const Color darkGrey = const Color(0xff707070);
+  static const Color grey = const Color(0xff8f8f8f);
+  static const Color lightGrey = const Color(0xfff5f5f5);
+  static const Color white = Colors.white;
+}
---
+++ lib/app/strings.dart
@@ -0,0 +1,85 @@
+class AppStrings {
+
+  //app name
+  static const String appName = "HYPO";
+
+  //page headlines
+  static const String accountPage = "Account";
+  static const String signUpPage = "Signup";
+  static const String settingsPage = "Settings";
+  static const String registerDevicePage = "Register Device";
+  static const String passwordRecoveryPage = "Password Recovery";
+  static const String monitorPage = "Monitor";
+  static const String homePage = "Home";
+  static const String controlPage = "Control";
+  static const String choosingPlantGroupPage = "Choose Plant Group";
+
+  //account page body
+  static const String yourInfo = "Your Information";
+  static const String changePassword = "Change Password";
+
+  //control page body
+  static const String waterPump = "Water Pump";
+  static const String lights = "Lights";
+
+  //home page body
+  static const String startDate = "Start date: ";
+  static const String daysPassed = "Days passed: ";
+  static const String endCycle = "End cycle";
+
+  static const String ph = "pH";
+  static const String ec = "EC";
+  static const String hours = "h";
+  static const String days = "d";
+  static const String weeks = "w";
+  static const String months = "m";
+
+  static const String noGrowingCycleMessage = "You don't have any plants growing yet. Press the start growing button to beign";
+
+  //login page body
+  static const String forgotPassword = "Forgot password?";
+  static const String newSignUp = "New here? Sign up!";
+
+  //monitor page body
+  static const String waterLevel = "Water Level";
+  static const String NutrientSolution = "Nutrinet Solution";
+  static const String phUpBuffer = "pH Up Buffer";
+  static const String phDownBuffer = "pH Down Buffer";
+  static const String centimeters = "cm";
+  static const String milliliters = "ml";
+
+  //register device page body
+  static const String deviceCode = "Device Code";
+
+  //settings page body
+  static const String account = "Edit your account";
+  static const String language = "Language";
+  static const String pushNotifications = "Push Notifications";
+  static const String help = "Help";
+  static const String about = "About";
+  static const String logout = "Logout";
+
+  //user info
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String email = "Email";
+  static const String password = "Password";
+  static const String confirmPassword = "Confirm Password";
+  static const String newPassword = "New Password";
+  static const String oldPassword = "Old Password";
+
+  //buttons
+  static const String saveChanges = "Save Changes";
+  static const String confirm = "Confirm";
+  static const String turnOnOff = "Turn On/Off";
+  static const String startGrowing = "Start Growing";
+  static const String login = "Login";
+  static const String recoverPassword = "Recover Password";
+  static const String signUp = "Signup";
+
+
+
+
+
+
+}
