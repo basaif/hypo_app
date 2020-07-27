e09bbc9e8cd136d993a6734e812156559de4bca1
basaif <iman.basaif@gmail.com>
2020-07-27 17:57:30 +0300

    Added icon

--- lib/ui/pages/login-page.dart
+++ lib/ui/pages/login-page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hypoapp/app/colors.dart';
 
 class loginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //TODO - create the login page
-    return Container();
+    return Container(
+      color: ColorSets.lightGrey,
+      child: Container(
+        color: ColorSets.white,
+      ),
+    );
   }
 }
