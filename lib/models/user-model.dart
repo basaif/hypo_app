793428c3e2d207137f6b20e859a097a78451c464
basaif <iman.basaif@gmail.com>
2020-07-31 15:33:20 +0300

    added user model

---
+++ lib/models/user-model.dart
@@ -0,0 +1,11 @@
+class UserModel{
+  String firstName;
+  String lastName;
+  String emailAddress;
+  String password;
+  String deviceCode;
+
+  UserModel(this.firstName, this.lastName, this.emailAddress, this.password,
+      this.deviceCode);
+
+}
