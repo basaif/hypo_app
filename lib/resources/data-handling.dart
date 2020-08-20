d108e59c1dc600ea45da8ec8e673dd03c2fe9469
basaif <iman.basaif@gmail.com>
2020-08-20 10:34:54 +0300

    implemented update device measurements

--- lib/resources/data-handling.dart
+++ lib/resources/data-handling.dart
@@ -57,6 +57,18 @@ class DataHandler{
 
   }
 
+  static Future<bool> getPlants() async{
+    http.Response response = await ApiServices.getPlants(http.Client());
+    if(response.statusCode == 200){
+      final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
+      PlantModel.plantsList = parsed.map<PlantModel>((json) => PlantModel.fromJson(json)).toList();
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
   static Future<bool> measurementsHandler(String deviceCode) async{
     http.Response response = await ApiServices.updateDeviceMeasurements(http.Client(), deviceCode);
     if(response.statusCode == 201){
@@ -74,16 +86,7 @@ class DataHandler{
 
   }
 
-  static Future<bool> getPlants() async{
-    http.Response response = await ApiServices.getPlants(http.Client());
-    if(response.statusCode == 200){
-      final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
-      PlantModel.plantsList = parsed.map<PlantModel>((json) => PlantModel.fromJson(json)).toList();
-      return true;
-    }
-    else{
-      return false;
-    }
 
-  }
+
+
 }
